93a2af9756bf4ba8847719778355c8ab9deee675
Lorenzo Bonanni <lorenzopuntobonanni@gmail.com>
2025-01-17 10:54:16 +0100

    Refactoring

--- mctsVoRos/loopHandler.py
+++ mctsVoRos/loopHandler.py
@@ -1,9 +1,9 @@
 from functools import partial
+import gc
 import os
 import pickle
 import random
 import threading
-from debug_utils import debug_plots_and_animations
 import rclpy
 import rclpy.qos
 import numpy as np
@@ -15,13 +15,14 @@ from MCTS_VO.bettergym.compiled_utils import check_coll_vectorized, dist_to_goal
 from MCTS_VO.environment_creator import create_pedestrian_env
 from sensorsHandler import SensorHandler
 from geometry_msgs.msg import Twist
-from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup
-from mcts_vo_ros_msgs.msg import Obstacles, RobotState
 from rclpy.node import Node
 from numba import jit
-from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
+from rclpy.executors import SingleThreadedExecutor
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 
-MAX_STEPS = 10
+
+MAX_STEPS = 50
 
 # X python = Unity Z
 # Z python = Unity Y 
@@ -30,9 +31,6 @@ MAX_STEPS = 10
 def set_seed(value):
     np.random.seed(value)
 
-def get_state(logger):
-    logger.info(f"RANDOM: {random.getstate()}\n\n")
-    logger.info(f"NP: {np.random.get_state()}\n\n")
 
 def seed_everything(seed_value: int):
     random.seed(seed_value)
@@ -53,27 +51,15 @@ class LoopHandler(Node):
 
     def __init__(self, dt):
         super().__init__('loopHandler')
+        self.dt = dt
+        self.logger = self.get_logger()
+        self.sensorHandler = SensorHandler(dt, self.logger)
 
         self.pub = self.create_publisher(Twist, 'cmd_vel', 1)
         # Subscribers for obstacles and position
-        self.callback_group = MutuallyExclusiveCallbackGroup()
-        self.state_sub = self.create_subscription(
-            RobotState, 
-            'state', 
-            self.callback_state, 
-            1, 
-            callback_group=self.reentrant_callback_group
-        )
-        self.lidar_subscriber = self.create_subscription(
-            Obstacles, 
-            'obstacle', 
-            self.callback_obs, 
-            1, 
-            callback_group=self.reentrant_callback_group
-        )
+        self.goal = np.array([-2.783, -0.993])        
         
         self.i = 0
-        self.dt = dt
         _, self.sim_env = create_pedestrian_env(
             discrete=True,
             rwrd_in_sim=True,
@@ -97,7 +83,6 @@ class LoopHandler(Node):
         # (obs_pos, obs_rad)
         self.obstacles = []
 
-        self.logger = self.get_logger()
 
         self.planner = Mcts(
             num_sim=100,
@@ -119,25 +104,58 @@ class LoopHandler(Node):
         self.times = []
         self.actions = []
         self.sim_env.gym_env.max_eudist = dist_to_goal(self.s0.goal, self.s0.x[:2])
-        self.robot_position = None
-        self.heading = None
-        self.obs_pos = None
-        self.obs_rad = None
         self.timer = self.create_timer(0., self.control_loop)
         self.logger.info('Loop Handler initialized')
+        self.time = 0
+        self.obs_pos = None
+        self.obs_rad = None
+        
+        self.odom_subscriber = self.create_subscription(
+            Odometry, 
+            'odom', 
+            self.sensorHandler.callback_odom, 
+            1
+        )
+        self.lidar_subscriber = self.create_subscription(
+            LaserScan, 
+            'scan', 
+            self.sensorHandler.callback_lidar, 
+            rclpy.qos.qos_profile_sensor_data
+        )
+        
+        seed_everything(0)
+
+    
+    def initialize(self):
+        # state, dist, angles, pos = self.get_state()
+        state = np.array([0.22631, -0.9885721, -3.14, 0.0])
+        self.s0, _ = self.sim_env.reset()
+        self.s0.goal = self.goal
+        obs = (
+            self.gt_obs_pos,
+            self.gt_obs_rad
+        )
+        self.s0.obstacles = obs
+        self.s0.x = state
+        self.trajectory = np.empty((0, 4))
+        self.planner.plan(self.s0)
+
+        self.pub.publish(Twist())
     
     def callback_state(self, msg):
+        self.logger.info("Callback State")
         self.robot_position = msg.position
         self.heading = msg.heading
     
     def callback_obs(self, msg):
+        self.logger.info("Callback Obs")
         self.obs_pos = np.array(msg.positions).reshape(msg.width, msg.height)
         self.obs_rad = np.array(msg.radii)
 
     def move(self, state, action, pub):
-        # check action 0: move forward 1: turn left 2: turn right
-        # save the linear velocity in target_linear_velocity
-        # save the angular velocity in target_angular_velocity
+        curr_time = time.time()
+        self.logger.info(f"Time MOVE: {curr_time-self.time}")
+        self.time = curr_time
         
         twist = Twist()
         twist.linear.x = action[0]
@@ -149,91 +167,80 @@ class LoopHandler(Node):
         d_theta = (action[1] - state[2] + np.pi) % (2 * np.pi) - np.pi
         twist.angular.z = d_theta/self.dt
         pub.publish(twist)
-        self.logger.info(f"Linear: {twist.linear.x} Anglular: {twist.angular.z}")
-
-    def initialize(self):
-        # state, dist, angles, pos = self.get_state()
-        seed_everything(0)
-        state = np.array([0.22631, -0.9885721, -3.14, 0.0])
-        self.s0, _ = self.sim_env.reset()
-        self.s0.goal = np.array([-2.783, -0.993])
-        obs = (
-            np.array([
-                [-1.127, -0.833, 0.0, 0.0],
-                [-0.92, -1.651, 0.0, 0.0],
-                # [-1.82, -0.306, 0.0, 0.0]
-            ]), 
-            np.array([0.1, 0.1])
-        )
-        self.s0.obstacles = obs
-        self.s0.x = state
-        self.trajectory = np.empty((0, 4))
-        self.planner.plan(self.s0)
-        self.pub.publish(Twist())
+        # self.logger.info(f"State: {state}")
+        # self.logger.info(f"Action: {action}")
+        # self.logger.info(f"Linear: {twist.linear.x} Anglular: {twist.angular.z}")
 
         
     def control_loop(self):
-        if self.obs_pos is None or self.robot_position is None:
+        initial_time = time.time()
+
+        if self.sensorHandler.obs_pos is None or self.sensorHandler.robot_position is None:
             return
+        
+        if self.i == 0:
+            # self.s0.obstacles = (self.sensorHandler.obs_pos, self.sensorHandler.obs_rad)
+            self.s0.obstacles = (self.gt_obs_pos, self.gt_obs_rad)  
+            
         self.logger.info(f"Step {self.i}")
 
         self.trajectory = np.vstack((self.trajectory, self.s0.x))
-        self.s0.obstacles = (self.obs_pos, self.obs_rad)
-        self.obstacles.append(self.s0.obstacles)
 
-        initial_time = time.time()
-        self.logger.info(f"State: {self.s0.x}")
-        self.logger.info(f"Obstacles: {self.s0.obstacles}")
-        # get_state(self.logger)
+        # self.logger.info(f"State: {self.s0.x}")
+        # self.logger.info(f"RANDOM: {random.getstate()}\n\n")
+        # self.logger.info(f"NP: {np.random.get_state()}\n\n")
+        # self.logger.info("Starting Planning")
         action, info = self.planner.plan(self.s0)
+        
         t = time.time() - initial_time
+        # self.logger.info(f"Action: {action}")
         self.logger.info(f"Time Elapsed: {t}")
-        time.sleep(self.dt - t)
+        self.move(self.s0.x, action, self.pub)
+        # time.sleep(self.dt - t)
         
         # DEBUG STUFF
-        self.actions.append(action)
-        self.infos.append(info)
+        # self.actions.append(action)
+        # self.infos.append(info)
         # END DEBUG STUFF
         
         # FORECAST NEXT STATE BASED ON ODOMETRY AND ACTION
-        position, heading = self.robot_position, self.heading
+        # position, heading = self.sensorHandler.robot_position, self.sensorHandler.heading
+        
+        position = self.s0.x[:2]
+        heading = self.s0.x[2]
         robot_state = np.array([position[0], position[1], heading, action[0]])
-
-        self.move(robot_state, action, self.pub)
-
+        
         self.s0.x = robot_dynamics(
             state_x=robot_state,
             u=action,
             dt=self.dt
         )
+        # self.s0.obstacles = (self.sensorHandler.obs_pos, self.sensorHandler.obs_rad)
+        self.s0.obstacles = (self.gt_obs_pos, self.gt_obs_rad)
+        self.obstacles.append(self.s0.obstacles)
         self.times.append(t)
+        
         self.i += 1
         d = dist_to_goal(self.s0.goal, self.s0.x[:2])
-        collision = check_coll_vectorized(self.s0.x[:2], self.gt_obs_pos[:, :2], 0.3, self.s0.obstacles[1])
+        collision = check_coll_vectorized(self.s0.x[:2], self.gt_obs_pos[:, :2], 0.2, self.s0.obstacles[1])
         if self.i == MAX_STEPS or d<=0.3 or collision:
             self.logger.info(f"Goal Reached: {d<=0.3} Collision: {collision}")
-            # pickle.dump(self.actions, open("debug/acts.pkl", 'wb'))
+            pickle.dump(self.actions, open("debug/acts.pkl", 'wb'))
+            pickle.dump(self.trajectory, open("debug/trj.pkl", 'wb'))
             # self.destroy_node()
-            raise Exception("Goal Reached")
-        
+            raise Exception("Finished")
 
 def main(args=None):
     dt = 0.3
+    gc.disable()
     rclpy.init(args=args)
     try:
-        loopHandler = LoopHandler(dt=dt)
-        sensorHandler = SensorHandler(dt=dt)
-        # executor = MultiThreadedExecutor(num_threads=6)  # Adjust the number of threads as needed
-        executor = SingleThreadedExecutor()  # Adjust the number of threads as needed
-        executor.add_node(sensorHandler)
-        executor.add_node(loopHandler)
-        # executor_thread = threading.Thread(target=executor.spin, daemon=True)
-        # executor_thread.start()
-        # executor_thread.join()
-        executor.spin()
+        executor2 = SingleThreadedExecutor()
+        executor2.add_node(LoopHandler(dt=dt))
+        executor2.spin()
         
     except Exception as e:
-        debug_plots_and_animations(loopHandler)
+        # debug_plots_and_animations(loopHandler)
         raise e
         # loopHandler.zero_vel()
         # rclpy.shutdown()
@@ -244,3 +251,5 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+    
+
--- mctsVoRos/sensorsHandler.py
+++ mctsVoRos/sensorsHandler.py
@@ -1,51 +1,75 @@
-import time
 import numpy as np
 from MCTS_VO.bettergym.compiled_utils import get_points_from_lidar
-import rclpy
 from sklearn.cluster import DBSCAN
-from rclpy.node import Node
-from turtlebot3 import TurtleBot3
-from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
-from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup
-from mcts_vo_ros_msgs.msg import Obstacles, RobotState
+import rclpy
+import tf_transformations
 import pyransac3d as pyrsc
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from rclpy.node import Node
 
 
 RADIUS_SCALE = 2.2
 
 
-class SensorHandler(Node):
-    def __init__(self, dt):
-        super().__init__('sensorHandler')
+class SensorHandler():
+    def __init__(self, dt, logger):
+        # super().__init__('SensorHandler')
         self.dt = dt
-        self.turtlebot3 = TurtleBot3(dt)
-        self.clusting_algo = DBSCAN(eps=0.1, min_samples=2, n_jobs=-1)
-
-        self.reentrant_callback_group = MutuallyExclusiveCallbackGroup()
-        self.odom_subscriber = self.create_subscription(
-            Odometry, 
-            'odom', 
-            self.callback_odom, 
-            1, 
-            callback_group=self.reentrant_callback_group
-        )
-        self.lidar_subscriber = self.create_subscription(
-            LaserScan, 
-            'scan', 
-            self.callback_lidar, 
-            rclpy.qos.qos_profile_sensor_data, 
-            callback_group=self.reentrant_callback_group
-        )
-        self.state_pub = self.create_publisher(RobotState, 'state', 1)
-        self.obstacles_pub = self.create_publisher(Obstacles, 'obstacle', 1)
-        self.logger = self.get_logger()
-        self.logger.info('Sensor Handler initialized')
+        self.clusting_algo = DBSCAN(eps=0.1, min_samples=3, n_jobs=-1)
         self.max_obs_vel = 0.
-        self.position = None
+        self.robot_position = None
         self.heading = None
+        self.obs_rad = None
+        self.obs_pos = None
+        self.lidar_msg = LaserScan()
+        self.odom_msg = Odometry()
+        # self.odom_subscriber = self.create_subscription(
+        #     Odometry, 
+        #     'odom', 
+        #     self.callback_odom, 
+        #     1
+        # )
+        # self.lidar_subscriber = self.create_subscription(
+        #     LaserScan, 
+        #     'scan', 
+        #     self.callback_lidar, 
+        #     rclpy.qos.qos_profile_sensor_data
+        # )
+        # self.state_pub = self.create_publisher(RobotState, 'state', 1)
+        # self.obstacles_pub = self.create_publisher(Obstacles, 'obstacle', 1)
+        # self.logger = self.get_logger()
+        self.logger = logger
+        self.logger.info('Sensor Handler initialized')
         
+    def SetLaser(self, msg):
+        self.lidar_msg = msg
 
+    def SetOdom(self, msg):
+        self.odom_msg = msg
+        
+    def get_odom(self):
+        # read odometry pose from self.odom_msg (for domuentation check http://docs.ros.org/en/noetic/api/nav_msgs/html/msg/Odometry.html)
+        point = self.odom_msg.pose.pose.position
+        rot = self.odom_msg.pose.pose.orientation
+        self.rot_ = tf_transformations.euler_from_quaternion([rot.x, rot.y, rot.z, rot.w])
+        heading = ((self.rot_[2]) + np.pi) % (2 * np.pi) - np.pi
+        return np.array([point.x, point.y]), heading
+
+    def get_scan(self):
+        distances = []
+        scan = self.lidar_msg.ranges
+        angle_min = self.lidar_msg.angle_min
+        angle_increment = self.lidar_msg.angle_increment
+
+        mask = np.where(~np.isinf(scan))[0]
+        scan = np.array(scan)
+
+        distances = scan[mask.astype(int)].copy()
+        angles = mask * angle_increment + angle_min
+
+        return distances, angles
+    
         
     def group_matrix(self, M, I):
         unique_indices = np.unique(I)
@@ -53,8 +77,10 @@ class SensorHandler(Node):
 
     
     def estimate_obstacles(self):
-        dist, angles = self.turtlebot3.get_scan()
-        pos = self.position
+        dist, angles = self.get_scan()
+        if len(dist) == 0:
+            return np.empty((0, 4)), np.array([])
+        pos = self.robot_position
         points = get_points_from_lidar(dist, angles, pos)
         clusters = self.clusting_algo.fit_predict(points)
         groups = self.group_matrix(points, clusters)
@@ -62,37 +88,37 @@ class SensorHandler(Node):
         obs_rad = np.array([])
         for group in groups.values():
             sph = pyrsc.Circle()
-            center, _ , radius, _ = sph.fit(group)
+            center, _ , radius, _ = sph.fit(group)            
             radius *= RADIUS_SCALE
             obs_pos = np.vstack((obs_pos, np.array([center[0], center[1], 0.0, self.max_obs_vel])))
             obs_rad = np.append(obs_rad, radius)
         
-        
         return obs_pos, obs_rad
     
     def callback_lidar(self, msg):
-        if self.position is None:
+        self.logger.info('Lidar callback')
+        if self.robot_position is None:
             return
             
-        self.turtlebot3.SetLaser(msg)
-        obs_pos, obs_rad = self.estimate_obstacles()
-
-        obs_msg = Obstacles(
-            positions=obs_pos.flatten(),
-            width=obs_pos.shape[0],
-            height=obs_pos.shape[1],
-            radii=obs_rad
-        )
+        self.SetLaser(msg)
+        self.obs_pos, self.obs_rad = self.estimate_obstacles()
         
-        self.obstacles_pub.publish(obs_msg)
+        # obs_msg = Obstacles(
+        #     positions=obs_pos.flatten(),
+        #     width=obs_pos.shape[0],
+        #     height=obs_pos.shape[1],
+        #     radii=obs_rad
+        # )
+        # self.logger.info(f'Obstacles: {obs_pos.shape[0]}')
+        # self.obstacles_pub.publish(obs_msg)
 
     def callback_odom(self, msg):
-        self.turtlebot3.SetOdom(msg)
-        self.position, self.heading = self.turtlebot3.get_odom()
-        self.state_pub.publish(
-            RobotState(
-                position=self.position,
-                heading=self.heading
-            )
-        )
-        
+        self.logger.info('Odom callback')
+        self.SetOdom(msg)
+        self.robot_position, self.heading = self.get_odom()
+        # self.state_pub.publish(
+        #     RobotState(
+        #         position=robot_position,
+        #         heading=heading
+        #     )
+        # )
